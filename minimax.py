111bfe7462e4e7fd8a4e69659891257766eba457
Cloud <cloudcr@gmail.com>
2024-10-03 18:02:59 +1300

    Add benchmark with random user

--- minimax.py
+++ minimax.py
@@ -1,6 +1,8 @@
 import math
-# from evaluation import EvaluationVersionOne, EvaluationVersionTwo
-from game_board import CellState
+from evaluation import EvaluationVersionOne, EvaluationVersionTwo
+from game_board import CellState, NineBoard, GameState
+from random_player import RandomPlayer
+
 
 class MiniMaxPlayer:
     def __init__(self, name, evaluation, default_depth=6):
@@ -91,3 +93,22 @@ class MiniMaxPlayer:
     
     def make_decision(self, board):
         return self.x_decision(board) if board.current_player == CellState.X else self.o_decision(board)
+    
+
+
+if __name__ == '__main__':
+    wins = 0
+    minimax_player = MiniMaxPlayer('AI', EvaluationVersionOne(), 4)
+    random_player = RandomPlayer('Random')
+    x_total_duration = 0
+    o_total_duration = 0
+    for _ in range(100):
+        board = NineBoard()
+        result, x_duration, o_duration = board.play_game(minimax_player, random_player)
+        if result == GameState.X_WIN:
+            wins += 1
+        x_total_duration += x_duration
+        o_total_duration += o_duration
+    print(f"Win rate for Minimax Player vs Random Player: {wins / 100 * 100}%")
+    print(f"Average duration for X: {x_total_duration / 100}")
+    print(f"Average duration for O: {o_total_duration / 100}")
