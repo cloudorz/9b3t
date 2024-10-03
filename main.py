e77735d0ea1b481a89c9b27a7b09fd33d0c646a4
Cloud <cloudcr@gmail.com>
2024-10-03 18:04:45 +1300

    remove unused code

--- main.py
+++ main.py
@@ -6,36 +6,11 @@ from random_player import RandomPlayer
 
 
 
-# def play_game(x_player, o_player):
-#     board = NineBoard()
-#     settings = {GameState.X_WIN: x_player.name, GameState.O_WIN: o_player.name}
-
-#     while not board.terminal():
-#         best_move = x_player.make_decision(board) if board.current_player == CellState.X else o_player.make_decision(board)
-
-#         if best_move:
-#             board.make_move(*best_move)
-
-#         # board.display_board()
-
-#     result = board.result()
-#     if result == GameState.DRAW:
-#         print("It's a draw!")
-#     else:
-#         print(f"{settings[result]} wins!")
-    
-#     return result
-
-
-
-
 if __name__ == '__main__':
     minimax_player_2 = MiniMaxPlayer('AI', EvaluationVersionTwo(), 6)
-    minimax_player = MiniMaxPlayer('AI 2', EvaluationVersionOne(), 4)
+    minimax_player = MiniMaxPlayer('AI 2', EvaluationVersionOne(), -1) # -1 is default depth for complete search
     random_player = RandomPlayer('Random')
-    mcts_player = MCTSPlayer('MCTS', 1, 1.4)
-    board = NineBoard()
-    result, x_duration, o_duration = board.play_game(mcts_player, random_player)
+    mcts_player = MCTSPlayer('MCTS', 0.5, 1.4)
+    board = NineBoard(lite=True)
+    result, x_duration, o_duration = board.play_game(mcts_player, minimax_player, True)
     print(f"Result: {result}, X Duration: {x_duration}, O Duration: {o_duration}")
-
-
